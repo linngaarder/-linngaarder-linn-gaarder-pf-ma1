9b4994120215bf0afc64494dde026546c9160b04
linngaarder <71817586+linngaarder@users.noreply.github.com>
2020-12-06 17:48:34 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+// Question 1
+
+var firstVar;
+
+// Question 2
+
+var name = "Linn";
+
+// Question 3
+
+var x = 7;
+
+// Question 4
+
+var division = 20/5;
+
+// Question 5
+
+var frog;
+typeof frog;
+
+// Question 6
+
+OrderHasShipped = true;
+console.log(OrderHasShipped);
+
+// Question 7
+
+OrderHasShipped = true;
+console.log(OrderHasShipped);
+
+if (OrderHasShipped === true) {
+    console.log ("true");
+} else {
+    console.log("false");
+}
+// Question 8
+
+for (var i = 0; i < 10; i += 1) {
+    console.log (i);
+    }
